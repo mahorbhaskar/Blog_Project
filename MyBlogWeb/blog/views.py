003476dc4567ccd288c4cbe39a8a909986a0636b
Bhaskar Mahor <bhaskarmahor1@gmail.com>
2021-09-14 12:48:02 +0530

    url and views

--- MyBlogWeb/blog/views.py
+++ MyBlogWeb/blog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from django.utils import timezone
+
 from blog.models import Post,Comment
 from blog.forms import PostForm,CommentForm
 from django.urls import reverse_lazy
@@ -47,3 +49,39 @@ class DraftListView(LoginRequiredMixin,ListView):
 
     def get_queryset(self):
         return Post.ojbects.filter(published_date__isnull=True).order_by('created_date')
+
+####################################################################################################
+####################################################################################################
+
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(post,pk=pk)
+    post.publish
+    return redirect('post_detail',pk=pk)
+
+@login_required 
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request,'blog/comment_form.html',{'form':form})
+
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(comment,pk=pk)
+    comment.approve()
+    return redirect('post_detail',pk=comment.post.pk)
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk=post_pk)
