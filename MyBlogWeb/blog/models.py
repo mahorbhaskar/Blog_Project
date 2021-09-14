57c0e1170ef62053d8dd3185add96c5b638eab4f
Bhaskar Mahor <bhaskarmahor1@gmail.com>
2021-09-14 10:38:17 +0530

    models-update

--- MyBlogWeb/blog/models.py
+++ MyBlogWeb/blog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
-
+from djanog.utils import timezone
+from django.core.urlresolvers import reverse
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True,null=True)
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+
+    def get_absolute_url(self):
+        return reverse("post_detail",kwargs={'pk':self.pk})
+    
+    def __str__(self):
+        return self.title
+
+
+class Comment(model.Model):
+    post = models.ForeignKey('blog.post',related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+        
+    def __str__(self):
+        return self.text 
